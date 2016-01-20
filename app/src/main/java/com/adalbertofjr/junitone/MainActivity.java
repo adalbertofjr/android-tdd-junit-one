6d988e83cee1c4b60acfc518d3a2b2ad55202afd
adalbertofjr <adalberto.fernandesjr@gmail.com>
2016-01-20 19:08:11 -0200

    Teste de configuração do junit.

---
+++ app/src/main/java/com/adalbertofjr/junitone/MainActivity.java
@@ -0,0 +1,16 @@
+package com.adalbertofjr.junitone;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by AdalbertoF on 20/01/2016.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
